5bd7338d4605b16369e6d98a6ede22b9527c841e
Julio Morell Padilla "Zero" <Zeronokami13@gmail.com>
2019-03-01 13:58:03 +0000

    PROYECT_KAMI: 	-TODO añadido para acordarme de POSMAN/POSTMAN

--- src/main/java/es/kami/controller/initController.java
+++ src/main/java/es/kami/controller/initController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class initController {
-
+		//TODO POSMAN!!!!
 		@GetMapping("/")
 		public String welcome() {
 			return "project/index";
